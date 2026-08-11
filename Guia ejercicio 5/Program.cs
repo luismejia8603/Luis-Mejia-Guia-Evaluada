73ae7fc999f9116050f9bc1b7fc979ff9dc9da46
luismejia8603 <137944291+luismejia8603@users.noreply.github.com>
2026-08-11 12:10:50 -0600

    Ejercicio 5

---
+++ Guia ejercicio 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cantidad de dinero en dolares (USD): ");
+            double dolares = double.Parse(Console.ReadLine());
+
+            Console.Write("Valor del cambio de moneda entre USD y Euro (0.86): ");
+            double  valorConversion = double.Parse(Console.ReadLine());
+
+            double euros = dolares * valorConversion;
+
+            Console.Write($"${dolares} dolares son: {euros} Euros");
+
+            Console.ReadKey();
+
+        }
+    }
+}
