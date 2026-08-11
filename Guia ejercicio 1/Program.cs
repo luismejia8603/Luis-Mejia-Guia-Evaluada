a90db54ba73b94abe0bac88a920fb0bb9622175c
luismejia8603 <137944291+luismejia8603@users.noreply.github.com>
2026-08-11 12:07:56 -0600

    Ejercicio 1

---
+++ Guia ejercicio 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar nombre del producto: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Precio: ");
+            double precio = double.Parse(Console.ReadLine());
+
+            double descuento = 0.10;
+
+            Console.Write("Cantidad de producto: ");
+            int cantidad = int.Parse(Console.ReadLine());
+
+
+            double subtotal = cantidad * precio;
+            double valorDescuento = descuento * subtotal;
+            double total = subtotal - valorDescuento;
+
+            Console.Clear();
+
+            Console.WriteLine($"Producto: {nombre}");
+            Console.WriteLine($"Subtotal: {subtotal:F2}");
+            Console.WriteLine($"Valor del descuento: {valorDescuento:F2}");
+            Console.WriteLine($"Total: {total:F2}");
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
