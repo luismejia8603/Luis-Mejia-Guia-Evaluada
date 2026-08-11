993fb95ace4a50f974485f27edfa7d555cf59195
luismejia8603 <137944291+luismejia8603@users.noreply.github.com>
2026-08-11 12:11:41 -0600

    Ejercicio 7

---
+++ Guia ejercicio 7/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar nombre del cliente: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingresar producto 1: ");
+            string producto1 = Console.ReadLine();
+
+            Console.Write("Ingresar precio: $");
+            double precio1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingresar producto 2: ");
+            string producto2 = Console.ReadLine();
+
+            Console.Write("Ingresar precio: $");
+            double precio2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingresar producto 3: ");
+            string producto3 = Console.ReadLine();
+
+            Console.Write("Ingresar precio:$");
+            double precio3 = double.Parse(Console.ReadLine());
+
+
+            double subTotal = precio1 + precio2 + precio3;
+
+            double iva = subTotal * 0.13;
+
+            double total = subTotal + iva;
+
+            Console.WriteLine($"{nombre} gracias por comprar");
+            Console.WriteLine($"Subtotal: ${subTotal:F2}");
+            Console.WriteLine($"IVA: ${iva:F2}");
+            Console.WriteLine($"Total a pagar: ${total:F2}");
+
+
+
+
+
+        }
+    }
+}
