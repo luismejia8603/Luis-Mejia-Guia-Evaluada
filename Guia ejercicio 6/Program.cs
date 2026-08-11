de3d15e446a3d186cf8edce9b718e212707d486f
luismejia8603 <137944291+luismejia8603@users.noreply.github.com>
2026-08-11 12:11:15 -0600

    Ejercicio 6

---
+++ Guia ejercicio 6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nombre empleado: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Salario base: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            Console.Write("Porcentaje de bonificación (%): ");
+            int porcentajeBono = int.Parse(Console.ReadLine());
+
+            double bono = salario * porcentajeBono/100;
+            double salarioFinal = salario + bono;
+
+            Console.WriteLine($"{nombre} tu bono es de ${bono}");
+            Console.WriteLine($"Tu salario final es de ${salarioFinal}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
