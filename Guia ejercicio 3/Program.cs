a9186a0128d12b14f4a362ecff1fe5ef0f84b742
luismejia8603 <137944291+luismejia8603@users.noreply.github.com>
2026-08-11 12:09:57 -0600

    Ejercicio 3

---
+++ Guia ejercicio 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar nombre empleado: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingresar horas trabajadas: ");
+            int horasTrabajadas = int.Parse(Console.ReadLine());
+
+            Console.Write("Pago correspondiente por hora: ");
+            double pagoXhora = double.Parse(Console.ReadLine());
+
+            double salario = horasTrabajadas * pagoXhora;
+
+
+            Console.Write($"Salario de {nombre}: {salario} ");
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
